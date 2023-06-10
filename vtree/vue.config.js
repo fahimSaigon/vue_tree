ddf0bb425c5bdc1ae168034f20a8d93051e61cff
fahim <fahim.saigon@gmail.com>
2023-06-10 23:53:53 +0800

    --

---
+++ vtree/vue.config.js
@@ -0,0 +1,18 @@
+'use strict'
+// Template version: 1.3.1
+// see http://vuejs-templates.github.io/webpack for documentation.
+
+const path = require('path')
+module.exports = {
+  // options...
+  devServer: {
+    proxy: {
+        '/': {
+            'target': 'http://127.0.0.1:8000/',
+            'changeOrigin': true,
+        }
+    }
+    
+    
+  }
+}
